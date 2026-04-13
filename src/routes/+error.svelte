dbf95a0a5a2329309a56f944db70ddd82994b077
Jan <janhu@gmx.net>
2026-04-13 18:36:55 +0200

    Added a basic error sveltekit page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<!-- src/routes/+error.svelte -->
+<script lang="ts">
+  import { page } from '$app/state';
+</script>
+
+<svelte:head>
+  <title>{page.status} - Page not found</title>
+</svelte:head>
+
+<div class="min-h-screen flex place-items-center">
+  <div class="text-center m-auto">
+    <h1 class="text-4xl font-bold">{page.status}</h1>
+    <p class="mt-2">{page.error?.message ?? 'Page not found'}</p>
+  </div>
+</div>
