b3364a8030844effd95eb6be714a290c0ac96943
Jan <janhu@gmx.net>
2026-04-20 18:04:38 +0200

    Changed Favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
+
 
 	let { children } = $props();
 </script>
