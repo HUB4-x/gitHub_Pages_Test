1bd6181bdce20a988b76e6fb1737e765bfa47785
Jan <janhu@gmx.net>
2026-05-09 23:31:10 +0200

    Added new lightmode theme

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -4,7 +4,7 @@
  */
 import { writable } from "svelte/store"
 
-const LIGHT_x_DARK_THEME: [string, string] = ['nord', 'business'] //maybe make this as an export var
+const LIGHT_x_DARK_THEME: [string, string] = ['corporate', 'business'] //maybe make this as an export var
 
 const THEME_LOCAL_STORAGE_KEY: string = 'theme-portfolio-page'
 
